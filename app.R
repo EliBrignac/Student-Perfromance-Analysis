6fb51689472bdda044ebc1993c6c3026735b13fc
Eli Brignac <94129362+EliBrignac@users.noreply.github.com>
2023-04-01 01:32:09 -0400

    Update app.R

--- app.R
+++ app.R
@@ -4,7 +4,7 @@ pacman::p_load(pacman, dplyr, GGally, ggplot2, ggthemes,
                  ggvis, httr, lubridate, plotly, rio, rmarkdown, shiny,
                  stringr, tidyr) 
 
-exams <- read.csv("Eli-app\\data\\exams.csv")
+exams <- read.csv("data\\exams.csv")
 
 exams <- exams %>%
   rename(ethnicity = race.ethnicity, 
